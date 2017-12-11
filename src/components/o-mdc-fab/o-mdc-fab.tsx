30560bb3ca2bb5cb965bd7cf607f11dfe28cecc6
RomuloCintra <romulo.cintra.contractor@bbva.com>
2017-12-11 14:41:05 +0100

    feat(o-mdc-fab) add ripple

--- src/components/o-mdc-fab/o-mdc-fab.tsx
+++ src/components/o-mdc-fab/o-mdc-fab.tsx
@@ -1,17 +1,44 @@
-import {Component, Prop} from '@stencil/core';
+import {Component, Prop, Element, CssClassMap} from '@stencil/core';
+import {MDCRipple} from '@material/ripple'
 
 @Component({
   tag: 'o-mdc-fab',
   styleUrl: 'o-mdc-fab.scss',
   shadow: true
 })
-export class MdcFabComponent {
 
+export class MdcFabComponent {
+  private ripple : any;
+
+  @Element()MdcFabEl : HTMLElement;
+  @Prop()name : string;
+  @Prop()id : string;
+  @Prop()href : string;
+  @Prop()mini : boolean;
+  @Prop()absolute : boolean;
+  @Prop()fixed : boolean;
+
+  componentDidLoad() {
+    this.ripple = MDCRipple.attachTo(this.MdcFabEl.shadowRoot.querySelector('.mdc-fab'));
+  }
+
+  componentDidUnload() {
+    this.ripple.destroy();
+  }
   render() {
+
+    const fabClasses : CssClassMap = {
+      'mdc-fab': true,
+      'mdc-fab--mini': this.mini,
+      'mdc-fab--absolute': this.absolute,
+      'mdc-fab--fixed': this.fixed
+    }
+
+    const TagType = this.href ? 'a' : 'button';
     return (
-      <button class="mdc-fab" aria-label="">
+      <TagType class={fabClasses} href={this.href} name={this.name} id={this.id}>
           <slot/>
-      </button>
+      </TagType>
     );
   }
 }
